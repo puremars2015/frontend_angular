9176fab8e7a3b7e0f8c205465c7a0352da9c9c98
Sean Ma <puremars2k10@gmail.com>
2024-12-02 12:32:57 +0800

    # 引用ngFor模板效果

--- my-app/src/app/about/about.component.ts
+++ my-app/src/app/about/about.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- my-app/src/app/index/index.component.ts
+++ my-app/src/app/index/index.component.ts
@@ -1,11 +1,35 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
 
+  articles = [
+    {
+        "image": "B0B0B0.png",
+        "title": "no article",
+        "description": "no article",
+        "link": "#",
+        "link_color": "purple"
+    },
+    {
+        "image": "B0B0B0.png",
+        "title": "no article",
+        "description": "no article",
+        "link": "#",
+        "link_color": "blue"
+    },
+            {
+        "image": "B0B0B0.png",
+        "title": "no article",
+        "description": "no article",
+        "link": "#",
+        "link_color": "pink"
+    }
+]
 }
