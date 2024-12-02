21f16269709b644855a104e84cb73fa97f2d1e90
Sean Ma <puremars2k10@gmail.com>
2024-12-02 14:13:33 +0800

    contact.html的搬移

--- my-app/src/app/contact/contact.component.ts
+++ my-app/src/app/contact/contact.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MessageService } from '../service/message/message.service';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  // Define the form group
+  contactForm:FormGroup = new FormGroup({
+    title: new FormControl(''),
+    content: new FormControl('')
+  }); 
+
+  constructor(private service: MessageService) { }
+
+  submitForm(event: any) {
+    console.log('Form submitted');
+    this.service.sendMessage(this.contactForm.value);
+  }
 }
---
+++ my-app/src/app/service/message/message.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor(private http: HttpClient) { }
+
+  sendMessage(message: any) {
+    // 呼叫api送出訊息
+    console.log('Message sent:', message);
+    this.http.post('http://localhost:5000/api/new_message', message).subscribe();
+  }
+
+}
